039c2bb2c75ad6fcb1f76c04ce8043ceb09d1459
Kjagan1907 <Kjagan304@gmail.com>
2025-10-06 00:33:43 +0200

    automatic mail when iss at current location

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 import requests
-from tkinter import *
+import datetime as dt
+import smtplib
+
+mail = "karumanchi304@gmail.com"
+password = "ypsh amsp eurv zknc"
+MY_LAT = 51.050407
+MY_LONG = 13.737262 # Your latitude and longitude
+
+parameters = {
+    'lat': MY_LAT,
+    'lng': MY_LONG,
+    'formatted': 0,
+}
+
+iss_response = requests.get('http://api.open-notify.org/iss-now.json')
+iss_response.raise_for_status()  # Ensure we got a successful response
+
+iss_data = iss_response.json()
+iss_latitude = float(iss_data['iss_position']['latitude'])
+iss_longitude = float(iss_data['iss_position']['longitude'])
 
 
-def get_quote():
-    pass
-    response = requests.get('https://api.kanye.rest')
-    response.raise_for_status()
-    kanye_quote = response.json()['quote']
-    canvas.itemconfig(quote_text, text=kanye_quote)
+sun_response = requests.get('https://api.sunrise-sunset.org/json?', params=parameters)
+sun_response.raise_for_status()  # Ensure we got a successful response
+sun_data = sun_response.json()
+sunrise = sun_data['results']['sunrise'].split('T')[1].split(':')[0]
+sunset = sun_data['results']['sunset'].split('T')[1].split(':')[0]
 
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
+current_time = dt.datetime.now().hour
 
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
-canvas.grid(row=0, column=0)
-
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
-
-
-
-window.mainloop()
+if (MY_LAT - 5) <= iss_latitude <= (MY_LAT + 5) and (MY_LONG - 5) <= iss_longitude <= (MY_LONG + 5):
+    if int(sunrise) >= current_time or current_time >= int(sunset):
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()  # Make the connection secure, encrypted
+            connection.login(user=mail, password=password)
+            connection.sendmail(
+                from_addr=mail,
+                to_addrs=mail,
+                msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
+            )  # \n\n is used to separate the subject from the body of the email
